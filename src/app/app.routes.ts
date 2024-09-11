c3ef4ef6801afc6702490ce46d0c514f0d964436
Stanislav Růžička <stanik.ruzicka@gmail.com>
2024-09-11 22:12:34 +0200

    added routing fixed the moving font

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {NONE_TYPE} from "@angular/compiler";
+import {BlogComponent} from "./blog/blog.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'blog', component: BlogComponent},
+];
