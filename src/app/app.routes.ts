b44267835c6a2d86aac20729a1e63e37ba9373e8
Stanislav Růžička <stanik.ruzicka@gmail.com>
2024-09-12 19:08:32 +0200

    day 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
-import {NONE_TYPE} from "@angular/compiler";
-import {BlogComponent} from "./blog/blog.component";
+import {BlogCardComponent} from "./blog/blog-card/blog-card.component";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'blog', component: BlogComponent},
+  { path: 'blog', component: BlogCardComponent},
 ];
---
+++ src/app/blog/tag.ts
@@ -0,0 +1,8 @@
+export const enum Tag {
+    PHYSICS = 'physics',
+    ART = 'art',
+    SCHOOL = 'school',
+    PROGRAMMING = 'programming',
+    PART_TIME= "part-time",
+    FREE_TIME ="free-time",
+}
