6e3db95edea3d11c1fabe205cb1192b2ccca5e72
Stanislav Růžička <stanik.ruzicka@gmail.com>
2024-12-14 17:42:39 +0100

    fixing images not loading, compressing static images

--- src/app/smaller_part/two-collum/two-collum.component.ts
+++ src/app/smaller_part/two-collum/two-collum.component.ts
@@ -37,9 +37,9 @@ export class TwoCollumComponent {
   tiles: TilesData[] = [
     {id: 1, side: 'left', title:"About me", text: "Hi I am Stanislav, I am a high school student at the Gymnázium a Střední odborná škola Rokycany, where i am studying IBDP. In my free-time I like to program and make stuff for and print on my 3D printer, basically engineering in general. \n" +
         "\n" + "I love to make engineering projects a while when I got into engineering I started by making trying to make remote controlled car, I made the chassis design, and failed, after being printed it did not fit together. So I made a 2nd, 3rd, 4th version then I got stuck on making circuits, I got over that, the only that stopped me was a motor that wasn´t powerful enough to propel the wheels. \n",
-      image: "/assets/me_photo.jpg"},
+      image: "assets/me_photo.jpg"},
     {id: 2, side: 'right', title:"Under Construction", text: "Lorem ipsum----> Brainstorming is a creative process that allows ideas to flow freely and generate innovative solutions. In a collaborative setting, individuals share their thoughts without fear of judgment, fostering an environment of open communication. The objective is to explore as many perspectives as possible, encouraging out-of-the-box thinking and unconventional approaches.",
-      image: "/assets/brainstorm.jpg"}
+      image: "assets/brainstorm.jpg"}
 
   ]
 
